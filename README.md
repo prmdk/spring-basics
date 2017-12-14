0fa44604debf9fcce19b2c62e96923c17d438c5a
prmdk <prmdk@users.noreply.github.com>
2017-12-14 19:10:44 +0530

    Spring Hello World

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-basics
 Just following some of the good spring tutorials!
+
+#Git Basics
+Also using repo to test git 
---
+++ SpringExamples/src/main/java/com/mkyong/common/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mkyong.common;
+
+/**
+ * Created by pkumar on 14/12/17.
+ */
+public class HelloWorld {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printHello() {
+        System.out.println("Hello ! " + name);
+    }
+}
